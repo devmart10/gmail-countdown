59c19623b3b33d77a22966174f53058992cce74b
devmart10 <devmart10@gmail.com>
2018-05-10 19:12:15 -0700

    cleans up main app code, and adds lots of comments. minor tweaks

--- GmailCountdown.js
+++ GmailCountdown.js
@@ -1,44 +1,76 @@
-setTimeout(function (){
-    var now = new Date();
+// global time constants (in millis)
+const one_hour = 3600000;
+const one_day = one_hour*24;
+const one_week = one_day*7;
+const now = new Date();
 
-    var all = document.getElementsByTagName("table");
-    var table = all[4];
-    var body = table.getElementsByTagName("tbody");
+/**
+ * Main script. Adds color to each row on current page based on
+ * the amount of time that's passed since the message was received
+ *
+ * @return {None}
+ */
+function colorTable() {
+    // html parsing
+    // TODO make more readable
+    let all = document.getElementsByTagName("table");
+    let table = all[4];
+    let body = table.getElementsByTagName("tbody");
+    let rows = body[0].getElementsByTagName('tr');
 
-    var rows = body[0].getElementsByTagName('tr');
+    // loop through rows and color them depending on the date received
     for (let i = 0; i < rows.length; i++) {
-        var span = rows[i].children[7].children[0];
-        var raw = span.title;
+        // parsing date from html table
+        // TODO make more readable
+        let span = rows[i].children[7].children[0];
+        let raw = span.title;
         raw = raw.split(',').splice(1).join('');
         raw = raw.replace('at ', '');
 
-        var date = new Date(raw);
-        var diff = now.getTime() - date.getTime();
-        // console.log(diff);
-        // 710 021 695
-        var one_day = 86400000;
-        var one_hour = 3600000;
-        var color = "";
-        if (diff < one_hour) {
-            color = "rgba(0, 0, 0, 0)";
-        }
-        else if (diff > one_day*7) {
-            color = "rgba(150, 0, 0, 1)";
-        }
-        else if (diff > one_day) {
-            color = "rgba(255, 0, 0, 1)";
-        }
-        else {
-            var perc = (diff - one_hour)/(one_day - one_hour);
-            console.log(perc);
-            var r = 255,
-                g = 0,
-                b = 0,
-                a = .1 + perc*.9;
+        // get color based on the date received
+        let received = new Date(raw);
+        let color = getColor(received);
 
-            // parseInt(perc*155)
-            color = `rgba(${r}, ${g}, ${b}, ${a})`;
-        }
+        // set the color
         span.parentElement.style.backgroundColor = color;
     }
-}, 2500);
+}
+
+/**
+ * Function that determines what the message should be colored
+ * 
+ * @param  {Date} received Date the message was received
+ * @return {String}          The color, as an rgba string
+ */
+function getColor(received) {
+    // calculate the time the message has been in inbox
+    let diff = now.getTime() - received.getTime();
+    
+    // less than an hour: no color
+    if (diff < one_hour) {
+        color = "rgba(0, 0, 0, 0)";
+    }
+    // less than a week: gradient
+    else if (diff < one_week) {
+        // calculate percent how close the message is to one week old
+        let perc = (diff - one_hour)/(one_week - one_hour);
+
+        // normalize alpha range to be in range 0 to .7 opacity
+        let alpha = perc*.7;
+        let r = 230,
+            g = 0,
+            b = 0,
+            a = alpha;
+
+        color = `rgba(${r}, ${g}, ${b}, ${a})`;
+    }
+    // over a week: dark red
+    else {
+        color = "rgba(180, 0, 0, .8)";
+    }
+    return color;
+}
+
+// use timeout to delay script because the gmail content needs to load
+const myDelay = 2500;   // 2.5 seconds
+setTimeout(colorTable, myDelay);
